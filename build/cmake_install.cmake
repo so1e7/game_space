cc6c6ee553ba9d97535c3e6a85df230aba015e0c
Егор <easimbirtsev@miem.hse.ru>
2021-06-03 17:33:50 +0300

    .

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/AiP/game_project
+# Install script for directory: C:/game_tennis
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/AiP/game_project/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/game_tennis/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
