0a0340a10b361da27657bdbd9cf2af6027114cb0
Егор <easimbirtsev@miem.hse.ru>
2021-06-17 00:02:58 +0300

    .

--- main.cpp
+++ main.cpp
@@ -4,42 +4,46 @@
 
 using namespace sf;
 
+//Основная функция приложения, в которой описана сама игра и взаимодействие между игроком и камнями
 int main()
 {
-    srand(time(0)); //Генерация случайного числа
+    srand(time(0)); 
     RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Space");
 
+    //Объявляем текстуру и спрайт для игрового фона
     Texture fon;
-    fon.loadFromFile(BACKGROUND_PATH);
+    fon.loadFromFile(BACKGROUND_PATH); //Загружаем фон игры в текстуру
     Sprite background(fon);
 
+    //Объявляем текстуру и спрайт для фона меню
     Texture t1;	
-	t1.loadFromFile(MENU_PATH);
+	t1.loadFromFile(MENU_PATH); //Загружаем фон игрового меню в текстуру
 	Sprite menu_background(t1);
 
-    //Игрок
+    //Объявляем текстуру и спрайт для игрока
     Texture p;
-    p.loadFromFile(PLAYER_PATH);
+    p.loadFromFile(PLAYER_PATH); //Загружаем картинку игрока(космического кораблся) в текстуру
     Sprite player(p);
     player.setTextureRect(IntRect(0, 0, 120, 90));
-    player.setPosition(500, 600);
+    player.setPosition(500, 600); //Устанавливаем начальную позицию корабля
 
-    //Снаряд
+    //Объявляем текстуру и спрайт для снаряда
     Texture bul;
-    bul.loadFromFile(BULLET_PATH);
+    bul.loadFromFile(BULLET_PATH);//Загружаем картинку снаряда в текстуру
     Sprite bullet(bul);
-    bullet.setPosition(-100, -100);
+    bullet.setPosition(-100, -100); //Устанавливаем начальную позицию снаряда
     bool s_bul = false; //коэффициент, который отвечает за отрисовку снаряда
     bullet.setScale(0.8f, 0.8f);
 
-    //Чтобы начать игру, нужно прожать "пробел"
+    //Объявляем текстуру и спрайт для кнопки, при помощи которой мы сможем начать игру
     Texture sp;
-    sp.loadFromFile(PROBEL_PATH);
+    sp.loadFromFile(PROBEL_PATH); 
     Sprite probel(sp);
-    probel.setPosition(200, 300);
+    probel.setPosition(200, 300); //Устанавливаем расположение надписи "Нажмите на пробел"
 
+    //Объявляем текстуру и спрайт для врагов, т.е. камней
     Texture en;
-    en.loadFromFile(ENEMY_PATH);
+    en.loadFromFile(ENEMY_PATH);//Загружаем картинку камня в текстуру
     Sprite enemy[6];
     int en_y[6] = {0}, e_x, e_y;
     for (int i = 0; i < 6; i++)
@@ -53,12 +57,14 @@ int main()
         enemy[i].setPosition(e_x, e_y);
     }
 
+    //Объявляем текстуру и спрайт для надписи "Очки"
     Texture scr;
     scr.loadFromFile(SCORE_PATH);
     Sprite score(scr);
-    score.setPosition(10, 5);
+    score.setPosition(10, 5); //Устанавливаем расположение надписи "Очки"
     score.setScale(0.4f, 0.4f);
 
+    //Объявляем текстуру и спрайт для цирф, которые используются для выведения счета
     Texture numb;
     numb.loadFromFile(NUMBER_PATH);
     Sprite number[3];
@@ -79,6 +85,7 @@ int main()
     {
         Vector2f pl = player.getPosition();
 
+        //Обработка очереди событий в цикле
         Event event;
         while (window.pollEvent(event))
         {
@@ -100,6 +107,7 @@ int main()
 
         if (play)
         {
+            //Обрабатка нажатия клавиш движения
             if (Keyboard::isKeyPressed(Keyboard::Right))
                 if (pl.x < 1080)
                     player.move(0.15f, 0);
@@ -127,6 +135,7 @@ int main()
                 enemy[i].move(0, 0.05);
                 em[i] = enemy[i].getPosition();
 
+                //Условие генерации нового камня, если он выходит за границу
                 if (em[i].y > 850)
                 {
                     en_y[i] = rand() % 5;
@@ -136,6 +145,7 @@ int main()
                     enemy[i].setPosition(e_x, e_y);
                 }
 
+                //Обрабатка столкновения космического кораблся с камнем
                 if (player.getGlobalBounds().intersects(enemy[i].getGlobalBounds()))
                 {
                     e_x = 220 * i;
@@ -144,7 +154,8 @@ int main()
 
                     n_k[2]--;
                 }
-
+                
+                //Обрабатка столкновения снаряда с камнем
                 if (bullet.getGlobalBounds().intersects(enemy[i].getGlobalBounds()))
                 {
                     en_y[i] = rand() % 5;
@@ -159,6 +170,7 @@ int main()
             }
         }
 
+        // Обрабатка счетчика очков
         if (n_k[2] > 9)
         {
             n_k[2] -= 10;
@@ -179,16 +191,16 @@ int main()
 
         window.clear(Color::White);
 
-        window.draw(background);
-        window.draw(player);
+        window.draw(background); //Отрисовывка игрового фона
+        window.draw(player); //Отрисовывка космического корабля
 
-        window.draw(bullet);
+        window.draw(bullet); //Отрисовывка снаряда
 
+        //Цикл, который отвечает за отрисовку камней
         for (int i = 0; i < 6; i++)
             window.draw(enemy[i]);
 
-        if (play)
-        {
+        if (play) {
             window.draw(score);
 
             for (int i = 0; i < 3; i++)
@@ -199,7 +211,7 @@ int main()
             window.draw(probel);
         }
 
-        window.display();
+        window.display(); //Отрисовка окна
     }
 
     return 0;
